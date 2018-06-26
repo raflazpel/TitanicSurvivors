77cd44437098ab27ea97cca0e6bda1a4235b58aa
r.lazcano.pello <rafalaz01@gmail.com>
2018-06-26 11:57:28 +0200

    Solved the problem with Nans, I will be using gender + age. Only need to add the test X and predict teh result

--- scripts/model.py
+++ scripts/model.py
@@ -4,25 +4,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.feature_selection import RFE
 from sklearn.linear_model import LogisticRegression
-from sklearn.cross_validation import train_test_split
+
 
 
 set = pd.read_csv("C:\\Users\\r.lazcano.pello\\Desktop\\Documentos no relacionados con el proyecto\\Python\\TitanicSurvivors\\Data\\cleanTrainingSet.csv")
+testSet = pd.read_csv("C:\\Users\\r.lazcano.pello\\Desktop\\Documentos no relacionados con el proyecto\\Python\\TitanicSurvivors\\Data\\test.csv")
+testSet = testSet.filter(items=['Age','Sex','Embarked'])
 
-ages = set.filter(items=['Survived', 'Age','Sex','Embarked'])
-ages = ages.dropna()
+
+trainingSet = set.filter(items=['Survived', 'Age','Sex','Embarked'])
+trainingSet = trainingSet.dropna()
+
 #Transform categorical data inot dummies
 
 cat_vars=['Sex','Embarked']
 for var in cat_vars:
     cat_list='var'+'_'+var
-    cat_list = pd.get_dummies(ages[var], prefix=var)
-    data1=ages.join(cat_list)
-    ages=data1
+    cat_list = pd.get_dummies(trainingSet[var], prefix=var)
+    data1=trainingSet.join(cat_list)
+    trainingSet=data1
 cat_vars=['Sex','Embarked']
-data_vars=ages.columns.values.tolist()
+data_vars=trainingSet.columns.values.tolist()
 to_keep=[i for i in data_vars if i not in cat_vars]
-data_final=ages[to_keep]
+data_final=trainingSet[to_keep]
 data_final.columns.values
 data_final_vars=data_final.columns.values.tolist()
 y=['Survived']
@@ -37,9 +41,44 @@ model =  sm.Logit(y,X)
 result=model.fit()
 print(result.summary())
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
 logreg = LogisticRegression()
-logreg.fit(X_train, y_train)
-y_pred = logreg.predict(X_test)
+logreg.fit(X, y)
+y_pred = logreg.predict(X)
 print(y_pred)
-print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+print('Accuracy of logistic regression classifier on training set: {:.2f}'.format(logreg.score(X, y)))
+
+
+#Transform categorical data inot dummies
+
+cat_vars=['Sex','Embarked']
+for var in cat_vars:
+    cat_list='var'+'_'+var
+    cat_list = pd.get_dummies(testSet[var], prefix=var)
+    data2=testSet.join(cat_list)
+    testSet=data2
+cat_vars=['Sex','Embarked']
+data_vars=testSet.columns.values.tolist()
+to_keep=[i for i in data_vars if i not in cat_vars]
+testData_final=testSet[to_keep]
+testData_final.columns.values
+data_final_vars=data_final.columns.values.tolist()
+
+
+X=[i for i in data_final_vars if i not in y]
+cols=["Age", "Sex_male","Sex_female"]
+X=testData_final[cols]
+X.fillna(25.0,inplace=True)
+
+print('wait')
+
+y_pred = logreg.predict(X)
+print('PREDICTION')
+print(y_pred)
+
+
+
+submission = pd.read_csv("C:\\Users\\r.lazcano.pello\\Desktop\\Documentos no relacionados con el proyecto\\Python\\TitanicSurvivors\\Data\\gender_submission.csv")
+submission.Survived=y_pred
+submission.to_csv('out2.csv',index=False)
+print(submission)
